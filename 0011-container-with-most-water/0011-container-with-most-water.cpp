d52dbeb332167d1f3327914eadd259fb4ea139d8
Sukirat Singh Monga <143656717+Sukirat15@users.noreply.github.com>
2023-09-06 20:35:34 +0530

    Update 0011-container-with-most-water.cpp

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -25,3 +25,21 @@ public:
         return max1;
     }
 };
+
+/*Clarifying Questions
+Can we slant the container or keep it straight??
+What if heights is empty??
+
+
+
+Approach:We will set two pointers l and r and calculate their height lh rh one which is minimum we will find area corresponding
+to that.one pointer which is at minimum height we will update only that...it can be either l or r and we will store the current max area in max.
+when l and r will cross each other we will exit the loop and return the max area*/
+
+
+
+Time Complexity:
+O(n)
+Space Complexity:
+O(1)
+    */
